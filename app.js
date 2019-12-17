d968dc47893ee707319450aedd1769bc21953e57
bnates <brian.nations@codefellows.com>
2019-12-16 20:19:16 -0800

    encapsulated code/refactored js

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-var hourNow = prompt('what time is it? 0-23');
-console.log('prompt question:', hourNow);
-
-var greeting;
-
-if (hourNow > 18 && hourNow < 24) {
-  greeting = 'good evening, everyone!';
-} else if (hourNow > 12 && hourNow < 24) {
-  greeting = 'good afternoon, everyone!';
-} else if (hourNow >= 0 && hourNow < 24) {
-  greeting = 'good morning, everyone';
-} else {
-  greeting = 'wait, you did not give me a proper response';
+function buildApp(msg, target) {
+  var hourNow = prompt(msg);
+  console.log('prompt question:', hourNow);
+  
+  var greeting;
+  
+  if (hourNow > 18 && hourNow < 24) {
+    greeting = 'good evening, ' + target;
+  } else if (hourNow > 12 && hourNow < 24) {
+    greeting = 'good afternoon, ' + target;
+  } else if (hourNow >= 0 && hourNow < 24) {
+    greeting = 'good morning, ' + target;
+  } else {
+    greeting = 'wait, you did not give me a proper response';
+  }
+  
+  console.log('our response:', greeting);
+  
+  document.write(greeting);
 }
 
-console.log('our response:', greeting);
-
-document.write(greeting);
+buildApp('what time is it in 24 hour timing', 'everyone');
